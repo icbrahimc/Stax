4c890e55a42efa1ac1bd44f55699942642c961fd
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-12-13 17:01:36 -0500

    Add the horizontal cells to the feature vc

--- Stax/Views/CategoryCell.swift
+++ Stax/Views/CategoryCell.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class CategoryCell: UICollectionViewCell {
+private let reuseIdentifier = "Cell"
+private let reuseIdentifierMusic = "musicCell"
+
+class CategoryCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
@@ -20,7 +23,7 @@ class CategoryCell: UICollectionViewCell {
     
     let playlistCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        
+        layout.scrollDirection = .horizontal
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .blue
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -30,9 +33,42 @@ class CategoryCell: UICollectionViewCell {
     func setupViews() {
         backgroundColor = .black
         
+        playlistCollectionView.dataSource = self
+        playlistCollectionView.delegate = self
+        
+        playlistCollectionView.register(MusicCardCell.self, forCellWithReuseIdentifier: reuseIdentifierMusic)
         addSubview(playlistCollectionView)
         
         playlistCollectionView.autoPinEdgesToSuperviewEdges()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifierMusic, for: indexPath) as! MusicCardCell
         
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: frame.height)
+    }
+}
+
+class MusicCardCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        print("Setup views")
+        backgroundColor = .red
     }
 }
