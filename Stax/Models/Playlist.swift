2c7f7d1f998c5ff053f9e55ea6e4c71f231afb99
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-12-14 21:26:30 -0500

    Add playlist model

---
+++ Stax/Models/Playlist.swift
@@ -0,0 +1,13 @@
+//
+//  Playlist.swift
+//  Stax
+//
+//  Created by icbrahimc on 12/14/17.
+//  Copyright © 2017 icbrahimc. All rights reserved.
+//
+
+import Foundation
+
+class Playlist: NSObject {
+    var name: String?
+}
