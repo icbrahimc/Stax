91a715ede044585355ecc34735d09a51680c66b9
Hailey Manuel <hmanuel@usc.edu>
2017-12-18 21:50:13 -0800

    added more class members to the user and playlist and created a few functions for those classes

--- Stax/Models/Playlist.swift
+++ Stax/Models/Playlist.swift
@@ -15,4 +15,21 @@ class Playlist: NSObject {
     var spotifyLink: String?
     var appleLink: String?
     var cloudLink: String?
+    var rating: Decimal = 0.0
+    var coverArtLink: String?
+    var songs: NSMutableArray?
+    var numRatings: Decimal = 0.0
+    var numFavorites: Decimal = 0.0
+    
+    func updateRating(newRating: Decimal) {
+        let totalRating = self.rating * self.numRatings + newRating;
+        self.numRatings = self.numRatings + 1;
+        self.rating = totalRating/self.numRatings;
+    }
+    
+    func incrementFavorites() {
+        self.numFavorites = self.numFavorites + 1;
+    }
 }
+
+
--- Stax/Models/User.swift
+++ Stax/Models/User.swift
@@ -11,4 +11,25 @@ import Foundation
 class User: NSObject {
     var name: String?
     var id: NSNumber?
+    var favoritedPlaylists: NSMutableArray?
+    
+    //not really sure how this function will work since user is not static
+    func getNameFromID(id: NSNumber?) -> String? {
+        return self.getName();
+    }
+    
+    func getName() -> String? {
+        return self.name;
+    }
+    
+    func favoritePlaylist(Playlist: NSObject) {
+        self.favoritedPlaylists?.add(Playlist);
+    }
+    
+    func unfavoritePlaylist(Playlist: NSObject) {
+        self.favoritedPlaylists?.remove(Playlist);
+    }
 }
+
+
+
