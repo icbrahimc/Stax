67e0305597c7f6b980ec4c211a3e2778aa69bfbc
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-12-20 13:54:48 -0800

    Setup the basic singleton struture

---
+++ Stax/API/BaseAPI.swift
@@ -0,0 +1,14 @@
+//
+//  BaseAPI.swift
+//  Stax
+//
+//  Created by icbrahimc on 12/20/17.
+//  Copyright © 2017 icbrahimc. All rights reserved.
+//
+
+import Foundation
+
+/* This is the base api class for the application. */
+class BaseAPI: NSObject {
+    static let sharedInstance = BaseAPI()
+}
