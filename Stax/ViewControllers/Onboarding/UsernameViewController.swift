41068c89e75378e371e0db09c20e714ba02e8112
icbrahimc <icbrahimc@Ibrahims-MBP.attlocal.net>
2017-12-26 00:56:26 -0800

    Add all UI components to the usernamevc

--- Stax/ViewControllers/Onboarding/UsernameViewController.swift
+++ Stax/ViewControllers/Onboarding/UsernameViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class UsernameViewController: UIViewController {
     let usernameField = UITextField.newAutoLayout()
-    let submitButton = UIButton.newAutoLayout()
+    let submitButton = UIButton(type: UIButtonType.roundedRect)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +28,38 @@ class UsernameViewController: UIViewController {
 extension UsernameViewController {
     func layout() {
         setupViews()
+        
+        usernameFieldSetup()
         usernameField.autoSetDimension(.height, toSize: 45)
         usernameField.autoSetDimension(.width, toSize: view.frame.width - 50)
-        usernameField.autoPinEdge(toSuperviewEdge: .top, withInset: 20)
+        usernameField.autoPinEdge(toSuperviewEdge: .top, withInset: 30)
         usernameField.autoAlignAxis(toSuperviewMarginAxis: .vertical)
+        
+        submitButtonSetup()
+        submitButton.autoAlignAxis(toSuperviewMarginAxis: .vertical)
+        submitButton.autoSetDimension(.height, toSize: 45)
+        submitButton.autoSetDimension(.width, toSize: view.frame.width / 2)
+        submitButton.autoPinEdge(.top, to: .bottom, of: usernameField, withOffset: 10)
+    }
+    
+    func usernameFieldSetup() {
+        usernameField.borderStyle = .roundedRect
+        usernameField.layer.cornerRadius = 10.0
+        usernameField.layer.borderWidth = 1.0
+        usernameField.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func submitButtonSetup() {
+        submitButton.backgroundColor = .black
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.setTitleColor(.white, for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        submitButton.layer.cornerRadius = 5.0
+        submitButton.layer.masksToBounds = true
     }
     
     func setupViews() {
         view.addSubview(usernameField)
+        view.addSubview(submitButton)
     }
 }
