5fd763a4746d7913e0186e2516d3cacff2316712
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-12-25 19:14:29 -0800

    Add the firestore to the project

---
+++ Pods/Target Support Files/Pods-Stax/Pods-Stax-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Stax : NSObject
+@end
+@implementation PodsDummy_Pods_Stax
+@end
---
+++ Pods/Target Support Files/Pods-StaxTests/Pods-StaxTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StaxTests : NSObject
+@end
+@implementation PodsDummy_Pods_StaxTests
+@end
