05ffaab22aaaa5f0909d4775d94d7a2e8588ef12
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-12-25 19:31:28 -0800

    Add the first api calls for the singleton

--- Stax/API/BaseAPI.swift
+++ Stax/API/BaseAPI.swift
@@ -6,8 +6,23 @@
 //  Copyright © 2017 icbrahimc. All rights reserved.
 //
 
+import FirebaseCore
+import FirebaseFirestore
 import UIKit
 
 class BaseAPI: NSObject {
     static let sharedInstance = BaseAPI()
+    var db: Firestore!
+    
+    override private init() {
+        let settings = FirestoreSettings()
+        Firestore.firestore().settings = settings
+        db = Firestore.firestore()
+    }
+    
+    /* Get the top rated playlists with a certain limit */
+    func getTopPlaylists(numPlaylists: Int) -> Query {
+        let playlists = db.collection("playlists");
+        return playlists.order(by: "rating", descending: true).limit(to: numPlaylists);
+    }
 }
