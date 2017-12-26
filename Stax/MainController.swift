155fd2d90274bda957e5d0f758f1994bf8625b54
icbrahimc <icbrahimc@Ibrahims-MBP.attlocal.net>
2017-12-25 23:41:55 -0800

    Add the mainVC to the maincontroller

--- Stax/MainController.swift
+++ Stax/MainController.swift
@@ -15,4 +15,23 @@ class MainController: NSObject, UITabBarControllerDelegate {
     fileprivate var selectedViewController: UIViewController?
     fileprivate var apiObserver: AnyObject?
     fileprivate var signoutObserver: AnyObject?
+    
+//    fileprivate lazy var welcomeViewController: UINavigationController = {
+//        NotificationCenter.default.addObserver(self, selector: #selector(MainController.dismissLoginFlow), name: NSNotification.Name(rawValue: "onboardingEnded"), object: nil)
+//        return EnrollNavigationController()
+//    }()
+    
+    var mainViewController: UIViewController {
+//        didInitializeWithToken = AuthorizationManager.sharedInstance.hasAccessToken()
+        if didInitializeWithToken {
+            return mainTabVC
+        }
+        
+        return WelcomeViewController()
+    }
+    
+    fileprivate func setup() {
+        self.mainTabVC.selectedIndex = 0
+        mainTabVC.delegate = self
+    }
 }
