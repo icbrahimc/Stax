4d7f074abfa89c603b44980e3f37ad4723b0775d
icbrahimc <icbrahimc@Ibrahims-MBP.attlocal.net>
2017-12-26 16:10:44 -0800

    Add profile manager to the project

---
+++ Stax/Manager/ProfileManager.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileManager.swift
+//  Stax
+//
+//  Created by icbrahimc on 12/26/17.
+//  Copyright © 2017 icbrahimc. All rights reserved.
+//
+
+import UIKit
+
+class ProfileManager: NSObject {
+    
+}
