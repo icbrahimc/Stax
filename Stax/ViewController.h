5e0096f0f4d547d0805f5e453ff054cc3aa65a16
Ibrahim <icbrahimc@gmail.com>
2018-01-03 00:07:53 -0800

    add spotify framework

---
+++ Stax/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Stax
+//
+//  Created by icbrahimc on 1/3/18.
+//  Copyright © 2018 icbrahimc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
