b069452f756990565dbb3edae47acdf19eb69a43
Ibrahim <icbrahimc@gmail.com>
2018-01-30 10:00:51 -0500

    Add new constants for spotify auth

--- Stax/Helpers/Constants.swift
+++ Stax/Helpers/Constants.swift
@@ -11,6 +11,10 @@ import Foundation
 struct Constants {
     static let APPLE = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkcyNjdDRjkyNTgifQ.eyJpc3MiOiIyQlo3NUdUMjJYIiwiaWF0IjoxNTE2NTU4NjE3LCJleHAiOjE1MTY2MDE4MTd9.aFjuK6GHwOywpo3uBLzM8duIuGFFihc3bORTEw_eOU1daO6zDe6FLp0lwuQbn50jndVyl_eKgswAZhkdIZJxCQ"
     
+    /* Spotify constants */
+    static let SPOTCLIENTID = "909d311de7ff4b9b84252482d9931598"
+    static let SPOTURL = URL(string: "stax://")!
+    static let SPOTSESSION = "current session"
 }
 
 
