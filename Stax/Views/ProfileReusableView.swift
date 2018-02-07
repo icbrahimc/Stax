c7d91663faf14427e8f8ba1e86cef3ea296ecd88
Ibrahim <icbrahimc@gmail.com>
2018-02-07 00:42:31 -0500

    Add restraints to the profile view ui

--- Stax/Views/ProfileReusableView.swift
+++ Stax/Views/ProfileReusableView.swift
@@ -9,33 +9,52 @@
 import UIKit
 
 class ProfileReusableView: UICollectionReusableView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .white
-    }
+    let nameLabelSize: CGFloat = {
+        switch UIScreen.main.nativeBounds.height {
+        case 1136:
+            return 22
+        case 1334:
+            // iPhones 6, 7, 8
+            return 24
+        case 2208:
+            // iPhones plus
+            return 28
+        case 2436:
+            // X
+            return 36
+        default:
+            return 32
+        }
+    }()
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Ibrahim Conteh"
-        label.font = UIFont.boldSystemFont(ofSize: label.font.pointSize)
-        return label
+    let numberSize: CGFloat = {
+        switch UIScreen.main.nativeBounds.height {
+        case 1136:
+            return 12
+        case 1334:
+            // iPhones 6, 7, 8
+            return 12
+        case 2208:
+            // iPhones plus
+            return 14
+        case 2436:
+            // X
+            return 16
+        default:
+            return 20
+        }
     }()
     
     let playlistNumberLabel: UILabel = {
         let label = UILabel()
         label.text = "15"
-        label.font = UIFont.boldSystemFont(ofSize: 12)
         return label
     }()
     
     let playlistLabel: UILabel = {
         let label = UILabel()
         label.text = "playlists"
-        label.font = UIFont.boldSystemFont(ofSize: 10)
+        label.font = UIFont.boldSystemFont(ofSize: 12)
         label.textColor = UIColor.init(white: 0.4, alpha: 0.6)
         return label
     }()
@@ -43,14 +62,14 @@ class ProfileReusableView: UICollectionReusableView {
     let followersNumberLabel: UILabel = {
         let label = UILabel()
         label.text = "640"
-        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.font = UIFont.boldSystemFont(ofSize: 14)
         return label
     }()
     
     let followersLabel: UILabel = {
         let label = UILabel()
         label.text = "followers"
-        label.font = UIFont.boldSystemFont(ofSize: 10)
+        label.font = UIFont.boldSystemFont(ofSize: 12)
         label.textColor = UIColor.init(white: 0.4, alpha: 0.6)
         return label
     }()
@@ -58,14 +77,14 @@ class ProfileReusableView: UICollectionReusableView {
     let followingNumberLabel: UILabel = {
         let label = UILabel()
         label.text = "780"
-        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.font = UIFont.boldSystemFont(ofSize: 14)
         return label
     }()
     
     let followingLabel: UILabel = {
         let label = UILabel()
         label.text = "following"
-        label.font = UIFont.boldSystemFont(ofSize: 10)
+        label.font = UIFont.boldSystemFont(ofSize: 12)
         label.textColor = UIColor.init(white: 0.4, alpha: 0.6)
         return label
     }()
@@ -73,14 +92,18 @@ class ProfileReusableView: UICollectionReusableView {
     let firstNameLabel: UILabel = {
         let label = UILabel()
         label.text = "Ibrahim"
-        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.numberOfLines = 1
         return label
     }()
     
     let lastNameLabel: UILabel = {
         let label = UILabel()
         label.text = "Conteh"
-        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.numberOfLines = 1
         return label
     }()
     
@@ -102,7 +125,7 @@ class ProfileReusableView: UICollectionReusableView {
     let editProfileButtton: UIButton = {
         let button = UIButton()
         button.setTitle("Edit Profile", for: .normal)
-        button.backgroundColor = .black
+        button.backgroundColor = UIColor(red:211.0/255.0, green:72.0/255.0, blue:54.0/255.0, alpha:1.0)
         button.setTitleColor(.black, for: .normal)
         return button
     }()
@@ -115,6 +138,17 @@ class ProfileReusableView: UICollectionReusableView {
         imageView.layer.masksToBounds = true
         return imageView
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     
     override func layoutSubviews() {
         addSubview(profilePic)
@@ -140,16 +174,18 @@ class ProfileReusableView: UICollectionReusableView {
         firstNameLabel.autoSetDimension(.width, toSize: 100)
         firstNameLabel.autoPinEdge(.left, to: .left, of: profilePic, withOffset: 5.0)
         firstNameLabel.autoPinEdge(.top, to: .bottom, of: profilePic, withOffset: 10.0)
+        firstNameLabel.font = UIFont.boldSystemFont(ofSize: nameLabelSize)
         
         lastNameLabel.autoSetDimension(.width, toSize: 100)
         lastNameLabel.autoPinEdge(.left, to: .left, of: firstNameLabel, withOffset: 0.0)
         lastNameLabel.autoPinEdge(.top, to: .bottom, of: firstNameLabel, withOffset: 0.0)
+        lastNameLabel.font = UIFont.boldSystemFont(ofSize: nameLabelSize)
         
         editProfileButtton.autoPinEdge(.top, to: .top, of: lastNameLabel)
         editProfileButtton.autoPinEdge(.left, to: .left, of: playlistLabel, withOffset: 0.0)
         editProfileButtton.autoSetDimension(.height, toSize: lastNameLabel.frame.height)
         editProfileButtton.autoSetDimension(.width, toSize: 100)
-        editProfileButtton.backgroundColor = .black
+        editProfileButtton.backgroundColor = UIColor(red:211.0/255.0, green:72.0/255.0, blue:54.0/255.0, alpha:1.0)
         
         usernameLabel.autoPinEdge(.top, to: .bottom, of: lastNameLabel)
         usernameLabel.autoPinEdge(.left, to: .left, of: lastNameLabel)
@@ -158,6 +194,7 @@ class ProfileReusableView: UICollectionReusableView {
         playlistNumberLabel.autoPinEdge(.top, to: .top, of: profilePic, withOffset: 0.0)
         playlistNumberLabel.autoPinEdge(.left, to: .right, of: profilePic, withOffset: 25.0)
         playlistNumberLabel.autoSetDimension(.width, toSize: 60)
+        playlistNumberLabel.font = UIFont.boldSystemFont(ofSize: numberSize)
         
         playlistLabel.autoPinEdge(.left, to: .left, of: playlistNumberLabel)
         playlistLabel.autoPinEdge(.top, to: .bottom, of: playlistNumberLabel, withOffset: 2.5)
@@ -166,6 +203,7 @@ class ProfileReusableView: UICollectionReusableView {
         followersNumberLabel.autoPinEdge(.bottom, to: .top, of: followersLabel, withOffset: 0.0)
         followersNumberLabel.autoPinEdge(.left, to: .left, of: playlistNumberLabel)
         followersNumberLabel.autoSetDimension(.width, toSize: 60)
+        followersNumberLabel.font = UIFont.boldSystemFont(ofSize: numberSize)
         
         followersLabel.autoPinEdge(.bottom, to: .bottom, of: profilePic)
         followersLabel.autoPinEdge(.left, to: .left, of: playlistLabel)
@@ -174,6 +212,7 @@ class ProfileReusableView: UICollectionReusableView {
         followingNumberLabel.autoPinEdge(.left, to: .left, of: followingLabel)
         followingNumberLabel.autoPinEdge(.bottom, to: .top, of: followingLabel, withOffset: 0.0)
         followingNumberLabel.autoSetDimension(.width, toSize: 60)
+        followingNumberLabel.font = UIFont.boldSystemFont(ofSize: numberSize)
         
         followingLabel.autoPinEdge(.bottom, to: .bottom, of: profilePic)
         followingLabel.autoPinEdge(.left, to: .right, of: followersLabel, withOffset: 25.0)
