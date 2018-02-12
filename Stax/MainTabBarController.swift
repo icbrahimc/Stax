c29f3e207ef8e739882351a347dff2d7d9156e92
Ibrahim <icbrahimc@gmail.com>
2018-02-12 10:50:28 -0500

    Hide all nav bars

--- Stax/MainTabBarController.swift
+++ Stax/MainTabBarController.swift
@@ -43,6 +43,7 @@ class MainTabBarController: UITabBarController {
         let navVCArtwork = UINavigationController(rootViewController: artworkVC)
         navVCArtwork.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "feed"), tag: 1)
         navVCArtwork.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+        navVCArtwork.isNavigationBarHidden = true
         
         let navVCSave = UINavigationController(rootViewController: SavedViewController())
         navVCSave.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "saveTab"), tag: 2)
@@ -52,14 +53,17 @@ class MainTabBarController: UITabBarController {
         let navVCSearch = UINavigationController(rootViewController: SearchViewController())
         navVCSearch.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "search"), tag: 3)
         navVCSearch.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+        navVCSearch.isNavigationBarHidden = true
         
         let navVCAddPlaylist = UINavigationController(rootViewController: NotificationsViewController())
         navVCAddPlaylist.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "notifications"), tag: 4)
         navVCAddPlaylist.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+        navVCAddPlaylist.isNavigationBarHidden = true
         
         let navVCProf = UINavigationController(rootViewController: profileVC)
         navVCProf.tabBarItem = UITabBarItem(title: nil, image: #imageLiteral(resourceName: "person"), tag: 5)
         navVCProf.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+        navVCProf.isNavigationBarHidden = true
         
         viewControllers = [navVCArtwork, navVCSave, navVCSearch, navVCAddPlaylist, navVCProf]
     }
